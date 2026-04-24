901db42cc32dce0c52289cdf99618eb81ec18a44
Aluno Fatec <aluno@fateclab.net>
2026-04-24 10:21:50 -0300

    adicao da tela de contato e finalizar pedido

--- Teste/Teste/obj/Debug/net8.0-windows/View/MainWindow.g.i.cs
+++ Teste/Teste/obj/Debug/net8.0-windows/View/MainWindow.g.i.cs
@@ -92,7 +92,7 @@ namespace Teste {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Teste;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Teste;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
